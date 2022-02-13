badca34ac1fe2373ff1738373fb861dd72fc4fd8
Jesus Martin <jesusmarzor28@gmail.com>
2022-02-13 13:11:10 +0100

    Actualizar descripcion sobre mi

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -13,9 +13,9 @@ export function About(){
                 <p className="about__p">Sobre mí</p>
             </div>
             <div className="about__content content">
-                <p className="content__p">Soy <strong>Jesús Martín Zorrilla</strong>, estudiante de <strong>Ingeniería Informática</strong>.</p>
-                <p className="content__p">Llevo estudiando desarrollo web más de 1 año. Me encantaría encontrar una empresa donde me sienta cómodo trabajando ya que es una de mis aficiones preferidas.</p>
-                <p className="content__p">Soy una persona amable, responsable, implicada y me encanta trabajar en equipo.</p>
+                <p className="content__p">Soy <strong>Jesús Martín Zorrilla</strong>, estudiante de <strong>Ingeniería Informática</strong> y vivo en Madrid.</p>
+                <p className="content__p">Llevo estudiando desarrollo web más de 1 año y paso gran parte de mi tiempo libre programando ya que es una de mis aficiones preferidas.</p>
+                <p className="content__p">Me considero una persona <strong>amable, responsable, organizada, implicada y me encanta trabajar en equipo</strong>.</p>
                 <div className="content__buttons">
                     <Button label="a" to="https://github.com/jesusmarzor">Proyectos</Button>
                     <Button label="download" download="CV-Jesus-Martin" to={cv} inverse={true} >Descargar CV</Button>
